1eee2ee490bd376040bb75521d42e79a0755cf24
Marnin Audu <msmaudu@gmail.com>
2022-04-21 16:53:47 +0100

    Created the nav and home page files

--- group46_restaurant_site/src/App.js
+++ group46_restaurant_site/src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import HomePage from './HomePage/homePage';
+import NavBar from './NavBar/navBar';
 
 function App() {
   return (
-    <></>
+    <>
+    <NavBar/>
+    <HomePage/>
+    {/* <AboutPage/>
+    <ProductPage/> */}
+
+    </>
   );
 }
 
---
+++ group46_restaurant_site/src/HomePage/homePage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './home.css'
+
+export default class HomePage extends Component {
+  render() {
+    return (
+      <div>
+          <h1>Welcome to the <strong id='BrownWord'>home</strong> of uncompromising <em id='BrownWord'>Variety</em> and <em id='BrownWord'>Quality</em> in cuisine</h1>
+      </div>
+    )
+  }
+}
